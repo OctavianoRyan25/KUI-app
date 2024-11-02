1a16df1149e1b06b02905df51462ae4106af015e
OctavianoRyan25 <octavianoryan25@gmail.com>
2024-11-02 17:27:48 +0700

    Edit detail form

--- resources/views/admin/letter.blade.php
+++ resources/views/admin/letter.blade.php
@@ -17,7 +17,7 @@
             <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
             </svg>
-            <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">Events</span>
+            <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">Letters</span>
             </div>
         </li>
         </ol>
@@ -25,7 +25,7 @@
     <div class="container mx-auto w-full px-4 py-8">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="font-bold text-3xl">Event</h1>
+                <h1 class="font-bold text-3xl">Letter</h1>
             </div>
         </div>
 
@@ -112,18 +112,22 @@
                             <td class="py-2 px-3 text-center">
                                 <div class="flex justify-center">
                                     @if($letter->file1 && $letter->file2 && $letter->file3)
-                                    <a href="{{ route('admin.letter.merge', $letter->id) }}" class="text-green-500 transition duration-200 hover:scale-110">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
-                                            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
-                                        </svg>
-                                    </a>
+                                        <a href="{{ route('admin.letter.merge', $letter->id) }}" class="text-green-500 transition duration-200 hover:scale-110">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                                                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+                                            </svg>
+                                        </a>
                                     @else
-                                    <p class="text-red-500">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                            <path d="M3.354 3.354a.5.5 0 0 1 .708 0L8 7.293l4.948-4.949a.5.5 0 0 1 .708.708L8.707 8l4.949 4.948a.5.5 0 0 1-.708.708L8 8.707l-4.948 4.949a.5.5 0 0 1-.708-.708L7.293 8 2.345 3.052a.5.5 0 0 1 0-.708z"/>
-                                        </svg>
-                                    </p>
+                                        <p class="text-red-500 relative group">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                                <path d="M3.354 3.354a.5.5 0 0 1 .708 0L8 7.293l4.948-4.949a.5.5 0 0 1 .708.708L8.707 8l4.949 4.948a.5.5 0 0 1-.708.708L8 8.707l-4.948 4.949a.5.5 0 0 1-.708-.708L7.293 8 2.345 3.052a.5.5 0 0 1 0-.708z"/>
+                                            </svg>
+                                            <!-- Tooltip -->
+                                            <span class="absolute top-full left-1/2 transform -translate-x-1/2 mt-1 hidden group-hover:block bg-gray-700 text-white text-xs rounded px-2 py-1 w-32">
+                                                File belum lengkap
+                                            </span>
+                                        </p>
                                     @endif
                                 </div>
                             </td>
